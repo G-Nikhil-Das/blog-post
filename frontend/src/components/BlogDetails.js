a806bf090d333f369e27a107bbb915b9cbda02cb
G-Nikhil-Das <Godaba.NikhilDas1@ltimindtree.com>
2023-10-17 17:50:32 +0530

    Updating posts with images

--- frontend/src/components/BlogDetails.js
+++ frontend/src/components/BlogDetails.js
@@ -28,7 +28,7 @@ const BlogDetails = () => {
           <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">{blogDetails.title}</h1>
           <p className="text-base mt-4 space-between font-semibold text-gray-600">{blogDetails.author.name} <br />Posted on <time>{format(new Date(blogDetails.createdAt), 'MMM d, yyyy HH:mm')}</time></p>
           <p className="mt-6 text-base leading-7 text-gray-600">{blogDetails.description}</p>
-          <img src={'http://localhost:5000/'+blogDetails.cover} alt="" className="pt-6"/>
+          <p className="flex justify-center pt-6"><img src={'http://localhost:5000/'+blogDetails.cover} alt="" /></p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
             {ismyBlog && 
               <Link to={`/blog/edit/${blogDetails._id}`} className="flex items-center rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
